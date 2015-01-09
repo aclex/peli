71450ec5d648e013fc6017a970e6aa2b1ba39c7f
Alexey Chernov <4ernov@gmail.com>
2015-01-10 01:06:58 +0300

    First tests for value_shell
    Tests for default constructor in value_shell and make_value()
    function were added.

---
+++ test/value_shell_make_value_string.cpp
@@ -0,0 +1,45 @@
+/*
+ * This file is part of Peli - universal JSON interaction library
+ * Copyright (C) 2014  Alexey Chernov <4ernov@gmail.com>
+ *
+ * Peli is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <cassert>
+#include <string>
+
+#include "peli/json/value.h"
+
+using namespace std;
+
+using namespace peli;
+
+int main(int argc, char* argv[])
+{
+	json::value v(json::make_value<string>());
+	json::value wv(json::make_value<wstring>());
+
+	try
+	{
+		string s(v);
+		wstring ws(wv);
+	}
+	catch(...)
+	{
+		return -1;
+	}
+
+	return 0;
+}
