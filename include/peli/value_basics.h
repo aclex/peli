5af83261deb02e11b88b8e3ba808e477be419611
Alexey Chernov <4ernov@gmail.com>
2019-08-01 02:30:20 +0300

    Add documentation for variant alias and its stuff

--- include/peli/value_basics.h
+++ include/peli/value_basics.h
@@ -1,3 +1,22 @@
+/*
+ * This file is part of Peli - universal JSON interaction library
+ * Copyright (C) 2014-2019  Alexey Chernov <4ernov@gmail.com>
+ *
+ * Peli is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef PELI_DETAIL_VALUE_BASICS_H
 #define PELI_DETAIL_VALUE_BASICS_H
 
@@ -9,8 +28,27 @@
 
 #include <peli/detail/template_snippets/copy_cv_reference.h>
 
+/** \brief Peli types and functions.
+ *
+ * This namespace contains an alias of variant type used for
+ * programming interaction with language constructions and
+ * some supplementary types and functions (e.g. exceptions).
+ */
 namespace peli
 {
+	/** \brief Alias of variant type used.
+	 *
+	 * Underlying variant type can be either `std::variant`
+	 * (by default, if C++17 support is available in the compiler)
+	 * or internal variant implementation.
+	 *
+	 * \tparam Types the types that may be stored in this variant.
+	 * All types must be (possibly cv-qualified) non-array object
+	 * types.
+	 *
+	 * \see `peli::detail::variant`
+	 * \see [std::variant](https://en.cppreference.com/w/cpp/utility/variant)
+	 */
 	template<typename... Types> using variant_type =
 #ifdef INTERNAL_VARIANT
 	detail::variant::variant
@@ -23,6 +61,23 @@ namespace peli
 		Types...
 	>;
 
+	/** \brief Wrapper for `visit` function.
+	 *
+	 * Used here mainly as it's not always possible to use types
+	 * derived from `std::variant` with `std::visit` directly.
+	 * Otherwise it's just a redirection.
+	 *
+	 * \tparam Visitor visitor type.
+	 * \tparam Variant variant type.
+	 *
+	 * \p vis visitor object.
+	 * \p v variant object.
+	 *
+	 * \return whatever visitor's `operator()` returns.
+	 *
+	 * \sa [std::visit](https://en.cppreference.com/w/cpp/utility/variant/visit)
+	 *
+	 */
 	template<class Visitor, class Variant>
 	constexpr decltype(auto) visit(Visitor&& vis, Variant&& v)
 	{
@@ -41,6 +96,18 @@ namespace peli
 		return ns::visit(vis, std::forward<std::remove_reference_t<variant_type>>(static_cast<variant_type>(v)));
 	}
 
+	/** \brief Checks if variant value is currently in empty state.
+	 *
+	 * Empty state meant as `std::monostate` holding in
+	 * `std::variant` or invalid state of internal variant.
+	 *
+	 * \tparam Ts types of variant type passed.
+	 *
+	 * \p v variant object to check.
+	 *
+	 * \see [std::monostate](https://en.cppreference.com/w/cpp/utility/variant/monostate)
+	 * \see [std::variant](https://en.cppreference.com/w/cpp/utility/variant)
+	 */
 	template<typename... Ts>
 	bool is_empty(const variant_type<Ts...>& v) noexcept
 	{
