b57291d527d60f256ced2f5a1418d77bf9fc30d6
Alexey Chernov <4ernov@gmail.com>
2022-03-16 01:34:07 +0300

    Update the example with new features

--- example/main.cpp
+++ example/main.cpp
@@ -3,6 +3,7 @@
 
 #include "peli/json/value.h"
 #include "peli/json/iomanip.h"
+#include "peli/json/find.h"
 
 using namespace std;
 using namespace peli;
@@ -11,6 +12,12 @@ int main(int, char**)
 {
 	ifstream input_file("input.json");
 
+	if (!input_file)
+	{
+		cerr << "Can't open input file." << endl;
+		return 1;
+	}
+
 	// load from input file stream
 	json::value v;
 	input_file >> v;
@@ -18,7 +25,7 @@ int main(int, char**)
 	if (!input_file.good())
 	{
 		cerr << "Input file is not correct JSON file." << endl;
-		return 1;
+		return 2;
 	}
 
 	// list all keys in root element, assuming root is object
@@ -28,8 +35,21 @@ int main(int, char**)
 		cout << p.first << '\n';
 	}
 
+	auto inl { json::value::parse("[\"I'm parsed inline\"]"s) };
+	get<json::object>(v)["inline"] = inl;
+
+	auto* const p { json::find_of<bool>(v, "/three"s) };
+
+	if (p)
+		*p = true;
+
 	// add another integer element to root object
 	get<json::object>(v)["addme"] = json::number { 42 };
+
+	const auto& dump { json::to_string(v, /*pretty*/ true) };
+
+	cout << "Content of the result:" << endl;
+	cout << dump << endl;
 
 	ofstream output_file("output.json");
 	// save to output file stream
@@ -38,6 +58,6 @@ int main(int, char**)
 	if (!output_file.good())
 	{
 		cerr << "Unable to save to output file." << endl;
-		return 2;
+		return 3;
 	}
 }
